0eaea1a3c7c823478709d07adb00ac0ba5233189
Beaury <beaury.valentin@gmail.com>
2019-11-20 16:58:57 +0100

    Fenêtre connexion

--- Connexion.Designer.cs
+++ Connexion.Designer.cs
@@ -28,20 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.loginTxt = new System.Windows.Forms.TextBox();
+            this.passwordTxt = new System.Windows.Forms.TextBox();
+            this.inscriptionLink = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginLbl = new System.Windows.Forms.Label();
+            this.mdpLbl = new System.Windows.Forms.Label();
+            this.loginBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // loginTxt
+            // 
+            this.loginTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginTxt.Location = new System.Drawing.Point(12, 120);
+            this.loginTxt.Name = "loginTxt";
+            this.loginTxt.Size = new System.Drawing.Size(526, 26);
+            this.loginTxt.TabIndex = 0;
+            this.loginTxt.KeyUp += new System.Windows.Forms.KeyEventHandler(this.loginTxt_KeyUp);
+            // 
+            // passwordTxt
+            // 
+            this.passwordTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTxt.Location = new System.Drawing.Point(12, 203);
+            this.passwordTxt.Name = "passwordTxt";
+            this.passwordTxt.PasswordChar = '*';
+            this.passwordTxt.Size = new System.Drawing.Size(526, 26);
+            this.passwordTxt.TabIndex = 1;
+            this.passwordTxt.KeyUp += new System.Windows.Forms.KeyEventHandler(this.passwordTxt_KeyUp);
+            // 
+            // inscriptionLink
+            // 
+            this.inscriptionLink.AutoSize = true;
+            this.inscriptionLink.Location = new System.Drawing.Point(9, 232);
+            this.inscriptionLink.Name = "inscriptionLink";
+            this.inscriptionLink.Size = new System.Drawing.Size(85, 13);
+            this.inscriptionLink.TabIndex = 2;
+            this.inscriptionLink.TabStop = true;
+            this.inscriptionLink.Text = "Créer un compte";
+            this.inscriptionLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.inscriptionLink_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(196, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Jeu de rôle";
+            // 
+            // loginLbl
+            // 
+            this.loginLbl.AutoSize = true;
+            this.loginLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginLbl.Location = new System.Drawing.Point(8, 97);
+            this.loginLbl.Name = "loginLbl";
+            this.loginLbl.Size = new System.Drawing.Size(63, 20);
+            this.loginLbl.TabIndex = 4;
+            this.loginLbl.Text = "Pseudo";
+            // 
+            // mdpLbl
+            // 
+            this.mdpLbl.AutoSize = true;
+            this.mdpLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mdpLbl.Location = new System.Drawing.Point(8, 180);
+            this.mdpLbl.Name = "mdpLbl";
+            this.mdpLbl.Size = new System.Drawing.Size(105, 20);
+            this.mdpLbl.TabIndex = 5;
+            this.mdpLbl.Text = "Mot de passe";
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginBtn.Location = new System.Drawing.Point(202, 273);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(141, 31);
+            this.loginBtn.TabIndex = 6;
+            this.loginBtn.Text = "Se connecter";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
             // 
             // Connexion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(545, 316);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.mdpLbl);
+            this.Controls.Add(this.loginLbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inscriptionLink);
+            this.Controls.Add(this.passwordTxt);
+            this.Controls.Add(this.loginTxt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Connexion";
             this.Text = "Se connecter";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox loginTxt;
+        private System.Windows.Forms.TextBox passwordTxt;
+        private System.Windows.Forms.LinkLabel inscriptionLink;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label loginLbl;
+        private System.Windows.Forms.Label mdpLbl;
+        private System.Windows.Forms.Button loginBtn;
     }
 }
 
--- Connexion.cs
+++ Connexion.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,88 @@ namespace Jeu_de_role
         public Connexion()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Vérifie que le couple login/mot de passe est correct en fonction de la réponse API.
+        /// </summary>
+        /// <param name="pseudo">Identifiant</param>
+        /// <param name="pass">Mot de passe</param>
+        /// <returns></returns>
+        public bool AskConnexion(string pseudo, string pass)
+        {
+            try
+            {
+                /* On Interroge l'api, et on définit si la connexion est réussie ou non. */
+                string jsonString = new WebClient().DownloadString("http://localhost/api/api/values/Connected?pseudo=" + pseudo + "&pass=" + pass).Replace("[", "").Replace("]", "");
+                if (jsonString == "true")
+                    return true;
+                else
+                    return false;
+            }
+            catch //Erreur -> on retourne faux
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Ouvre la fenetre menu
+        /// </summary>
+        public void OpenMenu()
+        {
+            /*TODO : Associer la fenêtre menu et l'ouvrir */
+
+
+            this.Close(); //On ferme la fenêtre de connexion.
+        }
+
+        public void OpenInscription()
+        {
+            /*TODO : Ouvrir la fenêtre d'inscription en modal */
+        }
+
+        public void TryValidation()
+        {
+            string login = loginTxt.Text;
+            string pass = passwordTxt.Text;
+
+            //On vérifie le couple login/mot de passe
+            if (AskConnexion(login, pass))
+            {
+                OpenMenu();
+                MessageBox.Show("Vous êtes connecté.");
+            }
+            else
+            {
+                MessageBox.Show("Pseudo ou mot de passe incorrect.");
+            }
+        }
+
+        /// <summary>
+        /// Bouton connexion cliqué. On essaie de se connecter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            TryValidation();
+        }
+
+        private void loginTxt_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+                TryValidation();
+        }
+
+        private void passwordTxt_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+                TryValidation();
+        }
+
+        private void inscriptionLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenInscription();
         }
     }
 }
