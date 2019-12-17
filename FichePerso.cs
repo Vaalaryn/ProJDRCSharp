74aa8730c7f1c81ef1e495faac0f1350d8aab7fd
Athyd <enzo.caron@viacesi.fr>
2019-12-17 17:49:32 +0100

    fiche perso (reste mise en page)

---
+++ FichePerso.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Data.Linq;
+using Jeu_de_role.Models;
+
+
+namespace Jeu_de_role
+{
+
+
+    public partial class FichePerso : Form
+    {
+        public string json;
+        public FichePerso()
+        {
+            InitializeComponent();
+            using (WebClient wc = new WebClient())
+            {
+                json = wc.DownloadString("file://10.176.131.132/Users/Elise/Documents/Watcher/Parties/Z3R5WL9pYuC0lHIk.json");
+                //const string V = "{ 'ID_PARTIE':'mgD6nsd5IN85IlRO','TITRE':'Test des fichier','DESCRIPTION_PARTIE':'Ok','image':[],'joueur':[{'utilisateur':{'ID_UTIL':1,'MAIL':'tata','PSEUDO':'toto','AVATAR':'MTIzNA=='},'personnage':[],'ID_JOUEUR':15,'ID_UTIL':1,'ID_PARTIE':'mgD6nsd5IN85IlRO','IS_MJ':true}]}";
+                //json = V;
+
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            //JObject myjson = JObject.Parse(json);
+
+            //DataSet data = JsonConvert.DeserializeObject<DataSet>(json);
+            //System.Diagnostics.Debug.WriteLine(json);
+            //System.Diagnostics.Debug.WriteLine('yolooooo');
+            dgv.DataSource = TraitementPersonnage.ListeReader(json);
+            //var jsonLinq = JArray.Parse(json);
+            
+
+        }
+
+       
+
+    }
+}
---
+++ Models/TraitementPersonnage.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Data.Linq;
+using Jeu_de_role.Classes;
+namespace Jeu_de_role.Models
+{
+    
+    class TraitementPersonnage
+    {
+        public static JToken tst ="";
+        public static JToken madata = "";
+        public static List<getFichePerso> ListeReader(string json)
+        {
+           
+            var myLinq = JObject.Parse(json);
+            var etap = JArray.Parse(myLinq["joueur"].ToString());
+            
+            foreach (JObject item in etap)
+            {
+                tst = item.GetValue("personnage");
+                
+            }
+            
+            foreach (JObject item in tst)
+            {
+                if ((bool)item["VIVANT"] == true){
+                    madata = item;
+                }
+                
+
+            }
+            var jsonLinq =madata;
+            List<getFichePerso> utilisateurs = new List<getFichePerso>();
+           
+
+            getFichePerso u = new getFichePerso();
+            u.ID_CLASSE = jsonLinq["ID_CLASSE"].ToString();
+            u.NOM = jsonLinq["NOM"].ToString();
+            u.PRENOM = jsonLinq["PRENOM"].ToString();
+            u.VIVANT = (bool)jsonLinq["VIVANT"];
+            u.DESCRIPTION = jsonLinq["DESCRIPTION"].ToString();
+            u.VIE = jsonLinq["VIE"].ToString();
+            u.MANA = jsonLinq["MANA"].ToString();
+            u.EXPERIENCE = jsonLinq["EXPERIENCE"].ToString();
+            u.NIVEAU = jsonLinq["NIVEAU"].ToString();
+
+
+
+            utilisateurs.Add(u);
+
+
+
+
+
+            return utilisateurs;
+
+        }
+    }
+}
