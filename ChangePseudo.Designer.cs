4d2e15f1829ae56e80d4ff665ae88bac779e5c51
Athyd <enzo.caron@viacesi.fr>
2019-12-20 11:07:01 +0100

    mdp cachés

--- ChangePseudo.Designer.cs
+++ ChangePseudo.Designer.cs
@@ -38,9 +38,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(2, 125);
+            this.button2.Location = new System.Drawing.Point(2, 102);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(123, 23);
+            this.button2.Size = new System.Drawing.Size(92, 19);
             this.button2.TabIndex = 11;
             this.button2.Text = "Annuler";
             this.button2.UseVisualStyleBackColor = true;
@@ -48,9 +49,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(143, 125);
+            this.button1.Location = new System.Drawing.Point(107, 102);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.Size = new System.Drawing.Size(89, 19);
             this.button1.TabIndex = 10;
             this.button1.Text = "Valider";
             this.button1.UseVisualStyleBackColor = true;
@@ -59,46 +61,52 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Location = new System.Drawing.Point(9, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.Size = new System.Drawing.Size(77, 13);
             this.label2.TabIndex = 9;
             this.label2.Text = "Mot de passe :";
             // 
             // label_mail
             // 
             this.label_mail.AutoSize = true;
-            this.label_mail.Location = new System.Drawing.Point(12, 7);
+            this.label_mail.Location = new System.Drawing.Point(9, 6);
+            this.label_mail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label_mail.Name = "label_mail";
-            this.label_mail.Size = new System.Drawing.Size(125, 17);
+            this.label_mail.Size = new System.Drawing.Size(96, 13);
             this.label_mail.TabIndex = 8;
             this.label_mail.Text = "Nouveau Pseudo: ";
             // 
             // mdp
             // 
-            this.mdp.Location = new System.Drawing.Point(12, 81);
+            this.mdp.Location = new System.Drawing.Point(9, 66);
+            this.mdp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.mdp.Name = "mdp";
-            this.mdp.Size = new System.Drawing.Size(250, 22);
+            this.mdp.PasswordChar = '*';
+            this.mdp.Size = new System.Drawing.Size(188, 20);
             this.mdp.TabIndex = 7;
             // 
             // pseudo
             // 
-            this.pseudo.Location = new System.Drawing.Point(12, 27);
+            this.pseudo.Location = new System.Drawing.Point(9, 22);
+            this.pseudo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pseudo.Name = "pseudo";
-            this.pseudo.Size = new System.Drawing.Size(250, 22);
+            this.pseudo.Size = new System.Drawing.Size(188, 20);
             this.pseudo.TabIndex = 6;
             // 
             // ChangePseudo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(273, 159);
+            this.ClientSize = new System.Drawing.Size(205, 129);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label_mail);
             this.Controls.Add(this.mdp);
             this.Controls.Add(this.pseudo);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ChangePseudo";
             this.Text = "Modifier pseudo";
             this.ResumeLayout(false);
