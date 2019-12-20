a3da7e6cb07f868bd8ec35973e6d035de6590f54
Beaury <beaury.valentin@gmail.com>
2019-12-20 10:33:04 +0100

    Start connexion

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Jeu_de_role
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new PartiePerso());
+            Application.Run(new Connexion());
         }
     }
 }
